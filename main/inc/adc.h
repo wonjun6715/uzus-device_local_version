aeeef67a3d36f73972aa872a9b8f11de224591d2
wonjun6715 <wj6715@naver.com>
2022-08-24 17:26:48 +0900

    Fixed untracked files

--- main/inc/adc.h
+++
@@ -1,7 +0,0 @@
-#ifndef __COMMON_H__
-#define __COMMON_H__
-
-void ADCInit();
-uint32_t GetADCValue(int channel);
-
-#endif
--- main/inc/gpio.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GPIO_H__
-#define __GPIO_H__
-
-#define BUILT_IN_LED            2
-#define LED_RELAY               22
-
-void GPIOInit();
-
-#endif
